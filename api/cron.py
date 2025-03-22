e6d4d620e8379cb6f492b59825c539954641bc55
unknown <pintusingh122000@gmail.com>
2025-03-23 00:19:47 +0530

    bug

--- api/cron.py
+++ api/cron.py
@@ -5,10 +5,9 @@ from main import scrape_full_month
 def handler():
     try:
         scrape_full_month()
-        return res.json({"message": result})
 
     except Exception as e:
-        return res.json({"message": f"Error: {str(e)}"}, status=500)
+        print(e)
 
 result = handler()
 print(result)
